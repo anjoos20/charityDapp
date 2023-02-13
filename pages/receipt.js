8e88fd1b3e24abf7154553bd0341a7621bd23070
Anju Balakrishnan <anjoos2006@gmail.com>
2023-02-13 13:49:08 +0530

    receipt diplsayed in ether

--- pages/receipt.js
+++ pages/receipt.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { abi, networks } from '../build/contracts/Funding.json'
+import { useState, useEffect } from "react";
 const Web3 = require('web3')
 
 const receipt = () => {
-        
-  const handleReceipt = async (event) => {
+    let [donor, setDonor] = useState();
+    let [donation, setDonation] = useState();
+    let amt =0;
+    
+    const handleReceipt = async (event) => {
         event.preventDefault()
         const web3 = new Web3(ethereum)
         const contractAddress = networks['5777'].address;
         const contractABI = abi;
         const contractInstance = new web3.eth.Contract(contractABI, contractAddress)
         const sender= event.target.sender.value;
-        let donAmount = 0;
+        
         alert(sender)
-
       contractInstance.getPastEvents("donateEvent", {
           filter: { sender: sender },
           fromBlock: 0,
@@ -25,26 +28,24 @@ const receipt = () => {
               events.forEach(function(event) {
                   console.log("Event:", event);
                   console.log("Donor address:", event.returnValues.sender);
-                  console.log("Donation amount:", event.returnValues.amount);
 
+                  console.log("Donation amount:", event.returnValues.amount);
+                  const etherValue = Web3.utils.fromWei(event.returnValues.amount, 'ether');
+                  let rAmount = parseInt(etherValue)
+                  console.log("ramount",rAmount)
+                  amt += rAmount;
+                  console.log("amount",amt)
               });
           }
+          setDonor(sender)
+          setDonation(amt)
       });
-        
-        //  contractInstance.events.donateEvent().on(
-        // 'data', function(event) {
-        //     console.log("hello")
-        //     console.log("event is",event);
-        //  }).on('error', console.error);
-
-        // Event1.get(function(err, result) {
-        // if (err) {
-        //     console.log(err)
-        //     return;
-        // }
-        // console.log("Found ", result);
-        // })
+    
     }
+
+    useEffect(() => {  
+    },[setDonor,setDonation]);
+      
   return (
     <div>
         <form className="form-inline" onSubmit={handleReceipt}>
@@ -56,15 +57,14 @@ const receipt = () => {
         <div className="card md-3 lg-3" style={{width: 600, margin:50}}>
             <div className="card-body">
                 <h5 className="card-title">Donation receipt from HelpUnlimited</h5>
-                <p className="card-text">Donor Address:</p>
-                <p className="card-text">Total donations till now in ether: </p>
+                <p className="card-text">Donor Address:{donor}</p>
+                <p className="card-text">Total donations till now in ether: {donation}</p>
                 <p className="card-text">We vlaue your donations. Thank you!</p>
             </div>
         </div>
     </div>
-
-    
   )
 }
+
 
 export default receipt
